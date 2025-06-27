11446b8bce3dfbb239dc2f848f05d7cb8e1cf347
Suru1496 <126154114+Suru1496@users.noreply.github.com>
2025-06-27 14:33:58 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+
+def train_graph_model(G, node_features, labels):
+    print("Model training not yet implemented. Add GNN here.")
+
+    # Plan: Use PyTorch Geometric or DGL to model compound-gene-disease graph
