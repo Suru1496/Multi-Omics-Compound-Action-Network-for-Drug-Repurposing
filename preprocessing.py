3c3ad2c3d46008a4fc50881bf9598787e0d2a7c5
Suru1496 <126154114+Suru1496@users.noreply.github.com>
2025-06-27 14:21:50 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import numpy as np
+
+def process_all():
+    # Dummy loading for now
+    expr_path = "data/raw/omics_expression.csv"
+    drug_path = "data/raw/drug_info.csv"
+    
+    print(f"Loading: {expr_path} and {drug_path}")
+    expression_df = pd.read_csv(expr_path)
+    drug_df = pd.read_csv(drug_path)
+
+    # Normalize gene expression
+    expression_df = (expression_df - expression_df.mean()) / expression_df.std()
+
+    return expression_df, drug_df
 
