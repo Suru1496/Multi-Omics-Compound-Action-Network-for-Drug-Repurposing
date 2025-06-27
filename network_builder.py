6de9859320478aee7818cfc8720d2157605d04eb
Suru1496 <126154114+Suru1496@users.noreply.github.com>
2025-06-27 14:29:38 +0530

    Update network_builder.py

--- network_builder.py
+++ network_builder.py
@@ -1,1 +1,19 @@
+import networkx as nx
+
+def build_heterogeneous_graph(expression_df, drug_df):
+    G = nx.Graph()
+
+    for gene in expression_df.columns:
+        G.add_node(gene, type="gene")
+
+    for _, row in drug_df.iterrows():
+        drug = row["drug_name"]
+        targets = row["targets"].split(";")
+
+        G.add_node(drug, type="compound")
+        for target in targets:
+            if target in expression_df.columns:
+                G.add_edge(drug, target, type="drug-target")
+
+    return G
 
